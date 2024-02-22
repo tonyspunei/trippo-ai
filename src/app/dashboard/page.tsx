28d4a56522ccc100a0daf38468012a51d34a77a4
Tony Spunei <tony.spunei@yahoo.com>
2024-02-22 06:13:16 +0100

    Update text

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ export default function Dashboard() {
   return (
     <div>
       <h1 className="text-xl">Dashboard Page</h1>
-      <p>Hello, this is the Trippo dashboard</p>
+      <p>This is the new and improved page!</p>
     </div>
   );
 }
