9ac83b5d67794d349366b911d911a7c9a42522c0
Tony Spunei <tony.spunei@yahoo.com>
2024-02-23 06:54:03 +0100

    use Container in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import Container from "@/components/Container";
+
 export default function Dashboard() {
   return (
     <div>
-      <h1 className="text-xl">Dashboard Page</h1>
-      <p>This is the new and improved page!</p>
+      <Container>
+        <h1 className="text-xl">Dashboard Page</h1>
+        <p>This is the new and improved page!</p>
+      </Container>
     </div>
   );
 }
