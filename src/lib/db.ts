c897066877ca107f3d027ef5c33867e88777353e
Tony Spunei <tony.spunei@yahoo.com>
2024-02-27 19:41:10 +0100

    fix type mismatch build error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { NeonQueryFunction, neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
 const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle(sql);
+export const db = drizzle(sql as unknown as NeonQueryFunction<boolean, boolean>);
