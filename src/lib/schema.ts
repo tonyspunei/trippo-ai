3bb8ca2c7a28a32171c3b2b7f2bc7fe0813a0459
Tony Spunei <tony.spunei@yahoo.com>
2024-03-06 07:54:36 +0100

    update db schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text }   from "drizzle-orm/pg-core";
+import { pgTable, text, integer, jsonb, timestamp }   from "drizzle-orm/pg-core";
 
 export const trips = pgTable("trips", {
   id: text("id").primaryKey(),
@@ -6,4 +6,14 @@ export const trips = pgTable("trips", {
   country: text("country").notNull(),
   descriptionShort: text("description_short").notNull(),
   imageURL: text("image_url").notNull(),
+  descriptionLong: text("description_long").notNull(),
+  flightMin: integer("flight_min").notNull(),
+  flightMax: integer("flight_max").notNull(),
+  flightTime: text("flight_time").notNull(),
+  hotel3: integer("hotel_3").notNull(),
+  hotel4: integer("hotel_4").notNull(),
+  hotel5: integer("hotel_5").notNull(),
+  tip: text("tip").notNull(),
+  itinerary: jsonb("itinerary").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),  
 })
