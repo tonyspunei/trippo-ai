b97ed73e1a578f2a3426f5148251e74b1252901e
Tony Spunei <tony.spunei@yahoo.com>
2024-03-06 07:44:07 +0100

    extends saveDestination logic & use new prompt to get more details

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,11 +1,12 @@
 "use server";
 
 import OpenAI from "openai";
-import { getGenerateUserMessage } from "./prompts";
+import { getGenerateUserMessage, getSaveDestinationUserMessage } from "./prompts";
 import { db } from "./db";
 import { trips } from "./schema";
 import { nanoid } from "nanoid";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -34,7 +35,7 @@ export const generateDestinations = async (_: GenerateState | null, formData: Fo
   const details = formData.get("details") as string;
 
   const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
+    model: "gpt-3.5-turbo-1106",
     messages: [
       {
         "role": "system",
@@ -81,9 +82,78 @@ async function fetchCityImage(query: string) {
   return json.photos[0].src.large as string;
 };
 
-export const saveDestination = async (city: string, country: string, descriptionShort: string, imageURL: string) => {
+// #1 Parse & Type our OpenAI Response
+// #2 Update our Database schema
+// #3 Push our (local) schema changes to our remote Neon DB
+// #4 Update our server action's db.insert() call
+// #5 Show something on the /trips/[id] page
+
+type OpenAiResponse = {
+  trip_description: string,
+  flight_price_min: number,
+  flight_price_max: number,
+  flight_time: string,
+  hotel_price: {
+    3: number,
+    4: number,
+    5: number
+  },
+  tip: string,
+  itinerary: {
+    day: number,
+    title: string,
+    description: string,
+    activities: string[]
+  }[]
+}
+
+export const saveDestination = async ({
+  city, 
+  country, 
+  descriptionShort, 
+  imageURL,
+  budget,
+  from,
+  days
+}: {
+  city: string;
+  country: string;
+  descriptionShort: string;
+  imageURL: string;
+  budget: number;
+  from: string;
+  days: number;
+}) => {
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo-1106",
+    messages: [
+      { "role": "system", "content": "You are a travel assistant." },
+      {
+        "role": "user",
+        "content": getSaveDestinationUserMessage({
+          city, 
+          country, 
+          details: descriptionShort, 
+          budget, 
+          from, 
+          days
+        })
+      }
+    ],
+    response_format: { type: "json_object" }
+  });
+
+  const content = response.choices[0].message.content;
+
+  if(!content) {
+    return null;
+  }
+
+  const tripDetails = JSON.parse(content) as OpenAiResponse;
+
+  const id = nanoid(14);
   await db.insert(trips).values({
-    id: nanoid(14),
+    id,
     city,
     country,
     descriptionShort,
@@ -91,4 +161,5 @@ export const saveDestination = async (city: string, country: string, description
   });
 
   revalidatePath("/dashboard");
+  redirect(`/trips/${id}`)
 }
