22f8103e56111a917fe154494271e3ee3524b7db
Tony Spunei <tony.spunei@yahoo.com>
2024-03-06 08:45:32 +0100

    add ItineraryList component

---
+++ src/app/trips/[id]/ItineraryList.tsx
@@ -0,0 +1,37 @@
+export type Itinerary = {
+  day: number,
+  title: string,
+  description: string,
+  activities: string[]
+};
+
+function ItineraryCard(props: Itinerary) {
+  return (
+    <div className="flex bortder rounded-xl py-8 px-6 shadow-lg">
+      <div className="flex flex-col space-y-2 w-1/2 pr-4">
+        <h2 className="text-2xl">Day {props.day}</h2>
+        <p className="italic font-bold">{props.title}</p>
+        <p>{props.description}</p>
+      </div>
+      <ul className="w-1/2 space-y-2 my-auto">
+        {props.activities.map((activity) => (
+          <li key={props.day+activity}>• {activity}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+type Props = {
+  itinerary: Itinerary[];
+}
+
+export default function ItineraryList(props: Props) {
+  return (
+    <div className="flex flex-col space-y-8 px-8">
+      {props.itinerary.map((day) => (
+        <ItineraryCard key={day.day+day.title} {...day} />
+      ))}
+    </div>
+  )
+}
