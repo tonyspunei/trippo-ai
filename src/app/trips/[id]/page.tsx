42c2f08617cdcbc3a11746c04e757d5532223892
Tony Spunei <tony.spunei@yahoo.com>
2024-03-06 08:10:15 +0100

    render Trip Details

--- src/app/trips/[id]/page.tsx
+++ src/app/trips/[id]/page.tsx
@@ -1,19 +1,22 @@
 import { db } from "@/lib/db";
 import { trips } from "@/lib/schema";
 import { eq } from "drizzle-orm";
+import TripDetail from "./TripDetail";
 
 type Props = {
   params: { id: string }
 }
 
-export default async function TripDetails(props: Props) {
+export default async function TripDetailPage(props: Props) {
   const trip = await db.select().from(trips).where(eq(trips.id, props.params.id));
 
   if(!trip[0]) return "No trip found";
 
   return (
-    <div>
-      <h1>You're going to {trip[0].city}</h1>
+    <div className="flex px-3 my-16 max-h-[150vh]">
+      <aside className="w-1/2 border-r-2 px-8 pb-24 overflow-y-auto">
+        <TripDetail {...trip[0]} />
+      </aside>
     </div>
   )
 }
